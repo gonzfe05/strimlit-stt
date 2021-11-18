ffa1d8fc09f8ab46a1b5a9803640f3524a6ea7c5
fernando <fernandogonzalez512@hotmail.com>
2021-11-18 04:07:09 +0000

    fixed endpoint

--- strimlit_app.py
+++ strimlit_app.py
@@ -58,21 +58,20 @@ def get_webrtc_context(key: str = "speech-to-text") -> WebRtcStreamerContext:
 
 def read_transcript(client: WebSocket, responses: List) ->List[str]:
     """Read from websocket"""
-    with st.empty():
-        while True:
-            # text_output = st.empty()
-            try:
-                response = client.recv()
-                response = json.loads(response)
-                if response['is_final']:
-                    text = f"{response['channel']['alternatives'][0]['transcript']} | is_final: {response['is_final']} | speech_final: {response['speech_final']}"
-                    # text_output.markdown(f"**Text:** {text}")
-                    st.write(text)
-                    responses.append(response)
-            except WebSocketDisconnect:
-                pass
-            except Exception:
-                pass
+    while True:
+        text_output = st.empty()
+        try:
+            response = client.recv()
+            response = json.loads(response)
+            if True:
+                text = f"{response['channel']['alternatives'][0]['transcript']} | is_final: {response['is_final']} | speech_final: {response['speech_final']}"
+                text_output.markdown(f"**Text:** {text}")
+                text_output = st.empty()
+                responses.append(response)
+        except WebSocketDisconnect:
+            pass
+        except Exception:
+            pass
 
 def start_read_thread(client: WebSocket, responses: List) -> None:
     """Init read thread"""
